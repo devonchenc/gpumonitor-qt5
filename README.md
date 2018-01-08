24e1b206ecc43dc177f230c525993bc4590dc6c0
tianrolin <tianrolin@163.com>
2018-01-08 12:54:46 +0800

    only allow single application instance

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-#GPUMonitor
+GPUMonitor
+==========
+
+This application is used to display NVIDIA GPUs' status, including device memory, GPU utilization, power and temperature. These status can also be seen in the system tray.
+
+These useful information of GPU comes from **'nvidia-smi'** command which is opened by **popen()** function. So, it dose **NOT** support **AMD** graphics card.
--- main.cpp
+++ main.cpp
@@ -2,20 +2,28 @@
 #include <QApplication>
 
 #include <QMessageBox>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
-        QMessageBox::critical(0, QObject::tr("Systray"),
-                              QObject::tr("I couldn't detect any system tray "
-                                          "on this system."));
+    if (!QSystemTrayIcon::isSystemTrayAvailable())
+    {
+        QMessageBox::critical(0, QObject::tr("GPUMonitor"),
+                              QObject::tr("Couldn't detect any system tray on this system."));
         return 1;
     }
+
+    QSharedMemory shared("GPUMonitor");
+    if (shared.attach())
+    {
+        return 0;
+    }
+    shared.create(1);
+
     QApplication::setQuitOnLastWindowClosed(false);
 
     MainWindow w;
-//    w.setWindowFlags(w.windowFlags() | Qt::WindowStaysOnTopHint);
     w.show();
 
     return a.exec();
